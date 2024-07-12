d061a8b676fd5c2aa209dcf7c3f1ba95f984965c
simultsop <22407+simultsop@users.noreply.github.com>
2024-07-12 08:07:38 +0200

    Create index.test.ts

---
+++ src/index.test.ts
@@ -0,0 +1,25 @@
+import { get, create, update, remove } from './index';
+
+describe('test get', () => {
+    it('should be defined', () => {
+        expect(get).toBeDefined();
+    });
+});
+
+describe('test create', () => {
+    it('should be defined', () => {
+        expect(create).toBeDefined();
+    });
+});
+
+describe('test update', () => {
+    it('should be defined', () => {
+        expect(update).toBeDefined();
+    });
+});
+
+describe('test remove', () => {
+    it('should be defined', () => {
+        expect(remove).toBeDefined();
+    });
+});
